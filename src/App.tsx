f697b6a1aa1b375b4348585d7a5210f473838d86
rhlinhares <raphael.linhares@gmail.com>
2022-10-13 18:55:36 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1 className='text-gray-800'>Hello World</h1>
+
+      <button className='bg-cyan-500'>Enviar</button>
+    </>
+  )
+}
+
